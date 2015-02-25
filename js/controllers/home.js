cc083bbf9f3dd88f5b42ca6ff43290be236b11b1
Dennis Ruhe <dennis@amazingsystems.nl>
2015-02-25 21:02:05 +0100

    Added map and worked on frontpage

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -10,10 +10,33 @@ angular.module('egulden.controllers', ['ngRoute'])
 }])
 
 .controller('homeController', ['$scope', '$http', function($scope, $http) {
-    $scope.blocks = [];
+    $scope.blocks     = [];
 
     $http.get('/data/blocks.json').then(function(response) {
         var data      = response.data;
         $scope.blocks = data.blocks;
     });
+
+    $scope.corePoints = [
+        {
+            icon        : 'signal',
+            title       : 'Sparen is belangrijk',
+            description : 'Als je niet spaart wordt je arm. Doe mee met het spaarprogramma'
+        },
+        {
+            icon        : 'home',
+            title       : 'Voor u, voor iedereen',
+            description : 'Wij vinden het heel belangrijk dat iedereen begrijpt hoe het werkt'
+        },
+        {
+            icon        : 'phone',
+            title       : 'Altijd en overal controle',
+            description : 'U heeft altijd en overal toegang tot uw geld'
+        },
+        {
+            icon        : 'qrcode',
+            title       : 'Eenvoudig betalen',
+            description : 'Betaal eenvoudig met uw mobiele telefoon'
+        }
+    ];
 }]);
